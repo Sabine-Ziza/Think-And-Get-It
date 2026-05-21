5eba8ab93bb0b48592ff6a02e0a7fb311143f937
Sabine Ingabire-test <ingabirezizas@gmail.com>
2026-05-21 14:25:40 +0200

    invalid login updated

--- src/test/java/Authentication/LoginTest.java
+++ src/test/java/Authentication/LoginTest.java
@@ -4,6 +4,7 @@ import base.BaseTest;
 import flows.LoginFlow;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import utils.ConfigLoader;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static org.testng.Assert.assertTrue;
@@ -20,8 +21,9 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void invalidLogin() {
-        loginPage.loginSection("invalidEmail", "invalidPassword");
+        loginPage.loginSection(ConfigLoader.get("invalidEmail"), ConfigLoader.get("invalidPassword"));
         assertThat(page).hasURL("https://think-and-get-it-frontend.onrender.com/login");
+        assertThat(page.locator(".go2072408551")).isVisible();
 
 
     }
